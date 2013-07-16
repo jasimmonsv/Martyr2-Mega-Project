73838137b84ede911e8c01a5782ce1098dcaf2a6
J.A. Simmons V <simmonsj@jasimmonsv.com>
2013-07-15 22:57:43 -0500

    initial commit

---
+++ python/Fibonacci.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+#Python Version 2.1
+#********************************
+#Author: J.A. Simmons V
+#http://www.wikihow.com/Calculate-the-Fibonacci-Sequence
+#
+#Fibonacci Sequence – Enter a number and have the program generate the 
+#Fibonacci sequence to that number or to the Nth number.
+#********************************
+
---
+++ python/pi.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+#Python Version 2.1
+
+#********************************
+#Author: J.A. Simmons V
+#http://mathworld.wolfram.com/PiFormulas.html
+#Find PI to the Nth Digit – Enter a number and have the program generate PI up to that 
+#many decimal places. Keep a limit to how far the program will go.
+#********************************
